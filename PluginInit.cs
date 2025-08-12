15882060312500fcc581d7414dfd9d39ad997d23
BrutalBonk <jieanbill@gmail.com>
2025-08-12 11:55:34 +0300

    [1.0.0] Upload

---
+++ PluginInit.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib.Tools;
+using UnityEngine;
+using HarmonyLib;
+using com.hokjhas.muck_patch.sdk;
+
+namespace com.hokjhas.muck_patch
+{
+    [BepInPlugin("com.hokjhas.muck_patch", "Plugin", "1.0.1")]
+    
+    public class Plugin : BaseUnityPlugin
+    {
+        Harmony Patch = new Harmony("com.hokjhas.muck_patch");
+        private void Awake()
+        {
+            Debug.Log("Loaded");
+            Patch.PatchAll();
+        }
+    }
+}
